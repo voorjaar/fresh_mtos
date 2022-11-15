31d253e8835633c4a0c9d74eedcbe39fef96bdfb
voorjaar <felitas@zoho.com>
2022-11-15 11:29:35 +0800

    feat: initial fresh plugin

---
+++ mod.ts
@@ -0,0 +1,44 @@
+import type { Plugin } from "$fresh/server.ts";
+import type { Config } from "./deps.ts";
+
+/**
+ * The type of plugin options.
+ */
+export type Options = Config & { selfURL: string };
+
+/**
+ * A wrapper function for defining mtos configuration.
+ * @param options - Plugin Options
+ */
+export function defineConfig(options: Options) {
+  return options;
+}
+
+/**
+ * The mtos plugin for Deno Fresh.
+ * @param options - Pluin options
+ */
+export function mtosPlugin(options: Options): Plugin {
+  return {
+    name: "mtos",
+    entrypoints: {
+      main:
+        `data:application/javascript,import options from "${options.selfURL}"; import {setup} from "${
+          new URL("./deps.ts", import.meta.url).href
+        }"; export default function(state){ setup(options) }`,
+    },
+    render(ctx) {
+      ctx.render();
+      return {
+        scripts: [
+          {
+            entrypoint: "main",
+            state: options,
+          },
+        ],
+      };
+    },
+  };
+}
+
+export default mtosPlugin;
